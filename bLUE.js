33ef0085a3eb831b4084236b5d3e42e8297086ed
Matt Hamada <ermacaz@gmail.com>
2023-04-07 15:56:40 -0700

    embed tweets in bLUE

---
+++ bLUE.js
@@ -0,0 +1,15 @@
+const links = document.querySelectorAll("div.message-contents p a");
+links.forEach((e) => {
+  if (e.href.indexOf("twitter.com") >= 0) {
+    let frame = document.createElement("iframe");
+    frame.setAttribute("data-s9e-mediaembed", "twitter");
+    frame.allow = "autoplay *";
+    frame.setAttribute("scrolling", "no");
+    frame.src = `https://s9e.github.io/iframe/2/twitter.min.html#${e.href.match(/status\/(\d+)/)[1]}${"d"}`;
+    frame.style = "background:url(https://abs.twimg.com/favicons/favicon.ico) no-repeat 50% 50%;border:0;height:250px;max-width:500px;width:100%";
+    frame.setAttribute("onload", "var c=new MessageChannel;c.port1.onmessage=function(e){style.height=e.data+'px'};contentWindow.postMessage('s9e:init','https://s9e.github.io',[c.port2])");
+    e.parentNode.insertBefore(frame, e.nextSibling);
+    e.className = "l";
+    // e.style.display = "none";
+  }
+})
