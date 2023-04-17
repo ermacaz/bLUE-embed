1b00306ec20fa667f1e0ae41f2459e96f735d1e1
ermacaz <ermacaz@protonmail.com>
2023-04-16 21:14:35 -0700

    streamable embeds

--- bLUE.js
+++ bLUE.js
@@ -9,11 +9,46 @@ links.forEach((e) => {
       frame.src = `https://s9e.github.io/iframe/2/twitter.min.html#${e.href.match(/status\/(\d+)/)[1]}${"d"}`;
       frame.style = "background:url(https://abs.twimg.com/favicons/favicon.ico) no-repeat 50% 50%;border:0;height:250px;max-width:500px;width:100%";
       frame.setAttribute("onload", "var c=new MessageChannel;c.port1.onmessage=function(e){style.height=e.data+'px'};contentWindow.postMessage('s9e:init','https://s9e.github.io',[c.port2])");
-      e.parentNode.insertBefore(frame, e);
+      var s = document.createElement("br");
+      e.parentNode.insertBefore(frame, e.nextSibling);
+      e.parentNode.insertBefore(s, e.nextSibling);
       e.className = "l";
-      e.style.display = "none";
     } catch {
       console.log("could not embed " + e.href)
+    }
+  } else if (e.href.indexOf("streamable.com") >= 0) {
+    try {
+      debugger;   
+      var streamSpan = document.createElement("span");
+			streamSpan.style.display = "inline"; 
+      streamSpan.style.position = "absolute";
+      // streamSpan.style.backgroundColor = s,
+
+      streamSpan.style.fontWeight = "bold";
+      streamSpan.style.textDecoration = "underline";
+      streamSpan.style.cursor = "pointer";
+      streamSpan.style.zIndex = 1;
+      streamSpan.className = "hide"; 
+      streamSpan.id = e; 
+      
+      streamSpan.innerHTML = "&nbsp[Hide]";
+      var s = document.createElement("br");
+			var a = document.createElement("div");
+			a.className = "streamable";
+			var i = document.createElement("iframe");
+      i.src = "https://www.streamable.com/e/" + e.href.match(/streamable.com\/(.*).*/)[1];
+      i.setAttribute("type", "text/html");
+      // i.setAttribute("width", 640);
+      i.setAttribute("height", 390);
+      i.setAttribute("autoplay", 0);
+      i.setAttribute("frameborder", 0);
+      i.setAttribute("allowfullscreen", "allowfullscreen")
+      var container = document.createElement('div')
+      e.parentNode.insertBefore(i, e.nextSibling);
+      e.parentNode.insertBefore(s, e.nextSibling);
+    } catch (error) {
+      console.log("could not embed " + e.href)
+      console.log(error)
     }
   }
 })
